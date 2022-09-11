520ac4ac99cf3d84305d0580cd13a1551d56d5b7
Favouredend <anosikefavour@gmail.com>
2022-09-11 06:11:46 -0700

    checking monty

--- main.c
+++ main.c
@@ -1,45 +1,96 @@
 #include "monty.h"
-bus_t bus = {NULL, NULL, NULL, 0};
 /**
-* main - monty code interpreter
-* @argc: number of arguments
-* @argv: monty file location
-* Return: 0 on success
-*/
+ * main - main function for monty interpreter
+ * @argc: arg count
+ * @argv: arg vector list
+ * Return: 0 on success or EXIT_FAILURE
+ */
 int main(int argc, char *argv[])
 {
-	char *content;
-	FILE *file;
-	size_t size = 0;
-	ssize_t read_line = 1;
+	char opcode[6] = {0}, wrong[1024] = {0};
+	ssize_t read;
+	size_t len;
 	stack_t *stack = NULL;
-	unsigned int counter = 0;
+	int matches = 0;
+	unsigned int line_number = 0;
 
-	if (argc != 2)
+	if (argc != 2)	/* if too many or too few arguments to monty exit fail */
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	file = fopen(argv[1], "r");
-	bus.file = file;
-	if (!file)
+	globes.fm = NULL;
+	globes.fm = fopen(argv[1], "r"); /* not freed must close file */
+	if (globes.fm == NULL)	/* if file cant open then exit fail */
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		fclose(globes.fm);
 		exit(EXIT_FAILURE);
 	}
-	while (read_line > 0)
+	globes.lineptr = NULL;
+	while ((read = getline(&globes.lineptr, &len, globes.fm)) != -1)
 	{
-		content = NULL;
-		read_line = getline(&content, &size, file);
-		bus.content = content;
-		counter++;
-		if (read_line > 0)
+		line_number++;
+		if (!_iswhitespace())
 		{
-			execute(content, &stack, counter, file);
+			matches = sscanf(globes.lineptr, "%s%d%1s", opcode, &globes.data, wrong);
+			if (matches != 2 && strcmp(opcode, "push") == 0)
+			{
+				fprintf(stderr, "L%d: usage: push integer\n", line_number);
+				exit_free(stack);
+				exit(EXIT_FAILURE);
+			}
+			if (opcode != NULL && opcode[0] != '#')
+				opcomp(&stack, line_number, opcode);
 		}
-		free(content);
+		free(globes.lineptr);
+		globes.lineptr = NULL;
 	}
+	exit_free(stack);
+	return (0);
+}
+
+/**
+ * free_stack - free a stack with single ptr reference
+ * @stack: pointer to a ll
+ * Return: void
+ */
+void free_stack(stack_t *stack)
+{
+	stack_t *temp;
+
+	while (stack != NULL)
+	{
+		temp = stack->next;
+		free(stack);
+		stack = temp;
+	}
+}
+/**
+ * exit_free - frees all possible allocs before exiting the program
+ * @stack: ptr to a stack
+ * Return: void
+ */
+void exit_free(stack_t *stack)
+{
+	fclose(globes.fm);
+	if (globes.lineptr != NULL)
+		free(globes.lineptr);
 	free_stack(stack);
-	fclose(file);
-return (0);
+}
+/**
+ * _iswhitespace - check for whitespace
+ *
+ * Return: 1 if only whitespace 0 if not only whitespace
+ */
+int _iswhitespace(void)
+{
+	int i, j;
+	char *ws = "\r\n\t ";
+
+	for (i = 0; globes.lineptr[i] != '\0'; i++)
+		for (j = 0; ws[j] != '\0' && ws[j] != globes.lineptr[i]; j++)
+			if (ws[j] == '\n')
+				return (0);
+	return (1);
 }
--- monty.h
+++ monty.h
@@ -1,12 +1,17 @@
-#ifndef MONTY_H
-#define MONTY_H
+#ifndef _MONTY
+#define _MONTY
+
+/* Libraries/Headers */
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <unistd.h>
+#include <sys/stat.h>
 #include <fcntl.h>
+#include <sys/types.h>
 #include <string.h>
-#include <ctype.h>
+
+int main(int argc, char *argv[]);
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -23,22 +28,6 @@ typedef struct stack_s
 	struct stack_s *next;
 } stack_t;
 /**
- * struct bus_s - variables -args, file, line content
- * @arg: value
- * @file: pointer to monty file
- * @content: line content
- * @lifi: flag change stack <-> queue
- * Description: carries values through the program
- */
-typedef struct bus_s
-{
-	char *arg;
-	FILE *file;
-	char *content;
-	int lifi;
-}  bus_t;
-extern bus_t bus;
-/**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
@@ -51,28 +40,40 @@ typedef struct instruction_s
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
-ssize_t getstdin(char **lineptr, int file);
-char  *clean_line(char *content);
-void f_push(stack_t **head, unsigned int number);
-void f_pall(stack_t **head, unsigned int number);
-void f_pint(stack_t **head, unsigned int number);
-int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
-void free_stack(stack_t *head);
-void f_pop(stack_t **head, unsigned int counter);
-void f_swap(stack_t **head, unsigned int counter);
-void f_add(stack_t **head, unsigned int counter);
-void f_nop(stack_t **head, unsigned int counter);
-void f_sub(stack_t **head, unsigned int counter);
-void f_div(stack_t **head, unsigned int counter);
-void f_mul(stack_t **head, unsigned int counter);
-void f_mod(stack_t **head, unsigned int counter);
-void f_pchar(stack_t **head, unsigned int counter);
-void f_pstr(stack_t **head, unsigned int counter);
-void f_rotl(stack_t **head, unsigned int counter);
-void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
-void addnode(stack_t **head, int n);
-void addqueue(stack_t **head, int n);
-void f_queue(stack_t **head, unsigned int counter);
-void f_stack(stack_t **head, unsigned int counter);
-#endif
+
+int _iswhitespace(void);
+void exit_free(stack_t *stack);
+void free_stack(stack_t *stack);
+void opcomp(stack_t **stack, unsigned int line_number, char *opcode);
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
+void mul(stack_t **stack, unsigned int line_number);
+void divide(stack_t **stack, unsigned int line_number);
+void mod(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
+void pstr(stack_t **stack, unsigned int line_number);
+void rotl(stack_t **stack, unsigned int line_number);
+void rotr(stack_t **stack, unsigned int line_number);
+
+
+
+/**
+ * struct GlobalStruct - contains all globals
+ * @data: int for push
+ * @fm: file stream for monty file
+ * @lineptr: pointer to char sting allocated by getline function
+ */
+struct GlobalStruct
+{
+	int data;
+	FILE *fm;
+	char *lineptr;
+} globes;
+
+#endif /* _MONTY */
